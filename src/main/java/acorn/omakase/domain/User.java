d5de41b33bd1a32f119a881fe1669fab7339ae86
daraekita <dj5668@naver.com>
2023-05-23 14:51:28 +0900

    docs.유저 수정
    domain.user 변수 추가
    userMapper getUserList 변경

--- src/main/java/acorn/omakase/domain/User.java
+++ src/main/java/acorn/omakase/domain/User.java
@@ -8,6 +8,10 @@ import lombok.Getter;
 public class User {
 
     private Long id;
-    private String loginId;
-
+    private String user_id;
+    private String user_name;
+    private String user_pw;
+    private String user_email;
+    private String user_region;
+    private String user_nickname;
 }
