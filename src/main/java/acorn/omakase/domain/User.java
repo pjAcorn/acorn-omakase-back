435b9bce5cbd2dd9c9fb836cac6a56cf4639d09f
daraekita <dj5668@naver.com>
2023-05-23 17:27:41 +0900

    docs:user.domain
    domain 유저 변수추가

--- src/main/java/acorn/omakase/domain/User.java
+++ src/main/java/acorn/omakase/domain/User.java
@@ -7,7 +7,12 @@ import lombok.Getter;
 @AllArgsConstructor
 public class User {
 
-    private Long id;
-    private String loginId;
+    private Long user_id;
+    private String login_id;
+    private String user_name;
+    private String user_pw;
+    private String user_email;
+    private String user_region;
+    private String user_nickname;
 
 }
