d6ef9d9925c30a3b701f661868d0a46b6dda572b
daraekita <dj5668@naver.com>
2023-06-05 11:25:24 +0900

    feat: mailConfirm 이메일인증
    이메일 인증 구현
    아이디,비밀번호 찾기에 이메일인증 추가

--- src/main/java/acorn/omakase/service/UserService.java
+++ src/main/java/acorn/omakase/service/UserService.java
@@ -7,10 +7,16 @@ import lombok.RequiredArgsConstructor;
 
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.thymeleaf.spring5.SpringTemplateEngine;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import java.util.List;
+import java.util.Random;
 
 @Slf4j
 @Service
@@ -18,6 +24,7 @@ import java.util.List;
 @Transactional
 public class UserService {
 
+    private final RedisUtil redisUtil;
     private final UserMapper userMapper;
 
     public List<User> getUserList() {
@@ -32,32 +39,45 @@ public class UserService {
     }
 
     // 아이디 찾기
-    public String findId(FindIdRequest findIdRequest){
+    public String findId(FindIdRequest findIdRequest) {
 
+        String email = findIdRequest.getEmail();
+        String redisEmail = redisUtil.getData(findIdRequest.getCode());
+
+        if (!email.equals(redisEmail)){
+            throw new IllegalStateException("인증번호 불일치");
+        }
         String id = userMapper.findId(findIdRequest);
 
-        User findId = User.of(findIdRequest);
-       
-
-        if(id== null){
-            throw new IllegalStateException("찾는 아이디가 없습니다.");
-        }
-        log.info("test");
         return id;
+    }
+
+    // 비밀번호 찾기
+    public String findPw(FindPwRequest findPwRequest){
+        String email = findPwRequest.getEmail();
+        String redisEmail = redisUtil.getData(findPwRequest.getCode());
+
+        if(!email.equals(redisEmail)){
+            throw new IllegalStateException("인증번호 불일치");
+        }
+
+        String pw = userMapper.findPw(findPwRequest);
+
+        return pw;
     }
 
     public User login(LoginRequest loginRequest) throws Exception {
         User userId = userMapper.login(loginRequest);
 
-        if(userId==null){
-             throw new Exception("아이디/비밀번호가 일치하지 않습니다.");
+        if (userId == null) {
+            throw new Exception("아이디/비밀번호가 일치하지 않습니다.");
         }
         return userId;
     }
 
 
     // 회원 탈퇴
-    public void delete(DeleteIdRequest deleteIdRequest){
+    public void delete(DeleteIdRequest deleteIdRequest) {
         DeleteIdRequest deleteUser = deleteIdRequest;
 
         Long userId = deleteUser.getUserId();
@@ -67,25 +87,28 @@ public class UserService {
         String password1 = deleteIdRequest.getPassword1();
         // 두번째 입력
         String password2 = deleteIdRequest.getPassword2();
-        if(password1.equals(password2 )){
-            if(password1.equals(password)){
+        if (password1.equals(password2)) {
+            if (password1.equals(password)) {
                 userMapper.deleteId(userId);
-            }else {
+            } else {
                 throw new IllegalStateException("비밀번호가 회원 정보와 틀립니다.");
             }
-        }else{
-             throw new IllegalStateException("입력한 두 비밀번호가 일치하지 않습니다.");
+        } else {
+            throw new IllegalStateException("입력한 두 비밀번호가 일치하지 않습니다.");
         }
     }
 
     // 아이디 중복 확인
-    public void idValidate(IdValidateRequest idValidateRequest){
+    public void idValidate(IdValidateRequest idValidateRequest) {
         User idChk = User.of(idValidateRequest);
 
         int check = userMapper.idValidate(idChk);
 
-        if(check > 0){
+        if (check > 0) {
             throw new IllegalStateException("중복된 아이디 입니다.");
         }
     }
+
+
+
 }
