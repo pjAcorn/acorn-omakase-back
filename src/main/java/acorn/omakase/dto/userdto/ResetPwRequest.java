3722665911f8e9afcb352bf333eb85f72b565b40
daraekita <dj5668@naver.com>
2023-06-10 16:56:11 +0900

    docs : WebConfig 8000 port
    8000번 포트 추가
    "http://api.localhost:8000/"

--- src/main/java/acorn/omakase/dto/userdto/ResetPwRequest.java
+++ src/main/java/acorn/omakase/dto/userdto/ResetPwRequest.java
@@ -8,7 +8,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ResetPwRequest {
+    private String pw;
     private String loginId;
     private String pw1;
     private String pw2;
+
+    //      패스워드 인코딩
+    public void encodingPassword(String password) {
+        this.pw = password;
+    }
 }
+
