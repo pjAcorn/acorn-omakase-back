2e2c344c021d35130bd89278f5c6c8e9a518a619
현경 <nhk1657@kpu.ac.kr>
2023-06-14 22:23:10 +0900

    feat: sending post request
    - 프론트에서 넘겨 준 데이터를 받아 파이썬 서버로 POST 요청 보냄
    - 그러나 NONE 보내지는 문제점 있어 해결 요망

---
+++ src/main/java/acorn/omakase/dto/analyzeDto/newAnalyzeRequest.java
@@ -0,0 +1,10 @@
+package acorn.omakase.dto.analyzeDto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter @Getter
+public class newAnalyzeRequest {
+    private String category;
+    private String address;
+}
